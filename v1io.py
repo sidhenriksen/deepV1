15fe5eeca8ba71d4384e6ea3673c3023b2d97db1
sidh0 <sid.henriksen@gmail.com>
2017-12-23 22:01:18 +0000

    Working piece of code for reading some input data

--- v1io.py
+++ v1io.py
@@ -15,17 +15,34 @@ class ExperimentData:
     def __init__(self,cellName):
 
         rawData = load_file(cellName)
+        base = rawData['NimCell'][0][0]
 
-        self.X = rawData['NimCell'][0][0][0]
-        self.spiketimes = rawData['NimCell'][0][0][1][0]
+        self.X = base[0]
+        self.spiketimes = base[1][0]
 
         dt = 0.01
-        self.y = np.histc(self.spiketimes,np.arange(self.X.shape[0])*dt
-        
-def get_data(runDur=30):
-    
+        self.y = np.histogram(self.spiketimes,np.arange(self.X.shape[0]+1)*dt)[0]
 
+        self.dxs = base[2][0]
+        self.times = base[3][0]
+        self.trials = base[4][0]
+        self.correlation = base[5][0]
+        self.duration = base[6][0]
+        self.contrast = base[7]
+
+    def get_data(self,runDur=30):
+        
+        idx = self.duration == runDur
+
+        X = self.X[idx,:]
+        y = self.y[idx]
+        return X,y
+        
 if __name__ == "__main__":
     
     cellName = 'lemM326c7.mat'
-    data = load_file(cellName)
+    data = ExperimentData(cellName)
+
+    X,y = data.get_data()
+
+    
